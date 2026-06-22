202a8584de81c2cecc4b41cef17f16eb111a005a
SinJungho <zmtls0516@naver.com>
2026-06-22 23:13:25 +0900

    fix(i18n): client.ts 내 누락된 useState 임포트 추가

--- src/i18n/client.ts
+++ src/i18n/client.ts
@@ -3,7 +3,7 @@
 import i18next from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import resourcesToBackend from "i18next-resources-to-backend";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
   initReactI18next,
   UseTranslationOptions,
